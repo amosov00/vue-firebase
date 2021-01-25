55e6cb14ef0989b00e4e4ac83a77a85645cceacd
themashina <amosov00@gmail.com>
2021-01-25 12:41:41 +0200

    test template

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,2 +1,23 @@
-import Registration from '@/views/Registration'
-console.log(Registration)
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuelidate from 'vuelidate';
+import VueRouter from 'vue-router'
+import Registration from "@/views/Registration";
+import Xbutton from "@/components/Xbutton";
+import Xcard from "@/components/Xcard";
+const localVue = createLocalVue()
+localVue.use(Vuelidate)
+localVue.use(VueRouter)
+localVue.component('x-btn', Xbutton)
+localVue.component('x-card', Xcard)
+
+
+describe('HelloWorld.vue', () => {
+    it('renders props.msg when passed', () => {
+        // const msg = 'new message'
+        const wrapper = shallowMount(Registration, {
+            localVue,
+        })
+        console.log(wrapper)
+        // expect(wrapper.text()).toMatch(msg)
+    })
+})
